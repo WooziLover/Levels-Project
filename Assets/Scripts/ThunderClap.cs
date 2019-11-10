2307be8d4d7188a360e2c4f343efe3e2ac9fd399
WooziLover <oohty15@gmail.com>
2019-11-10 13:15:14 -0800

    1/2 Thunder Code 11/10/19

---
+++ Assets/Scripts/ThunderClap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderClap : MonoBehaviour
+{
+   /* Light light = GetComponent<Light>();
+    bool canFlicker = true;
+
+    void Update()
+    {
+        StartCoroutine(Flicker());
+    }
+
+    IEnumerator Flicker()
+    {
+        if (canFlicker)
+        {
+            canFlicker = false;
+
+            light.enabled = true;
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.4f));
+            light.enabled = false;
+            yield return new WaitForSeconds(Random.Range(0.1f, 5));
+            canFlicker = true;
+        }
+    }*/
+}
